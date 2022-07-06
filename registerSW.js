2e787581d8ef2fd01113d5313d704416783966a1
JohnTing <jot.johnting@gmail.com>
2022-07-06 17:31:45 +0800

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-consultation-client-public/sw.js', { scope: '/vue-consultation-client-public/' })})}
